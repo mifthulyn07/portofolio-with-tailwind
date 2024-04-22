9ec17730546574241aea7f9678399f6fcef7ecba
mifthulyn07 <mifthulyn07@gmail.com>
2024-04-22 14:50:36 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ["index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
