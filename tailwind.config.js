03a96dd0f275eae396a85d389ec619b2886e6834
mifthulyn07 <mifthulyn07@gmail.com>
2024-04-23 17:34:56 +0700

    making hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   // content: ["./src/**/*.{html,js}"],
   content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      paddding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#f59e0b",
+        dark: "#020617",
+      },
+      screen: {
+        "2xl": "132px",
+      },
+    },
   },
   plugins: [],
 };
